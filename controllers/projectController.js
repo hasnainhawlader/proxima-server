0d3892b23cea92e6c41e26acf4785ce76ee42549
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-02-21 22:35:30 +0600

    before auth

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -78,11 +78,50 @@ const postProject = async (req, res) => {
 
 const updateProject = async (req, res) => {
   const { id } = req.params;
+
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Invalid id" });
   }
 
-  const porject = await Project.findOneAndUpdate({ _id: id }, { ...req.body });
+  const porject = await Project.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  );
 
   if (!porject) {
     return res.status(400).json({ error: "not find" });
