5b39ca367af95e362b9a9763d7a13449c4af05a3
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-02-25 22:35:25 +0600

    jwt

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,5 +1,11 @@
 const User = require("../models/userModel");
 
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, "process.env.SECRAT", { expiresIn: "7d" });
+};
+
 // login user
 
 const loginUser = async (req, res) => {
@@ -12,6 +18,13 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
+
+    // create token
+
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+
+    res.status(200).json({ user });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -15,6 +17,25 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  // validation
+
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  // check if the email is valid
+
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+
+  // lowercase, uppercase, number, symbol, 8+ chars
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password is not strong, try to combine uppercase, lowercase, number, symbol and minimum of 8 chars"
+    );
+  }
+
   const exist = await this.findOne({ email });
   if (exist) {
     throw Error("Email already used");
